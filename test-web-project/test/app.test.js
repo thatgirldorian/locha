74d765c637112e459f18ac885882b0ab6b680304
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-06-09 23:39:50 +0400

    added some error handling for when the text does not match

--- test-web-project/test/app.test.js
+++ test-web-project/test/app.test.js
@@ -25,3 +25,26 @@ it ('this shows a success message if email is valid', async() => {
 
     assert.strictEqual(h1.innerHTML, 'Looks great!')
 })
+
+it ('this has a text input', async () => {
+    const dom = await render('index.html')
+
+    const input = dom.window.document.querySelector('input')
+
+    assert(input)
+}) 
+
+//this tests whether the failed message is shown 
+it ('this shows a fail message if email is not valid', async() => {
+    const dom = await render('index.html')
+
+    const input = dom.window.document.querySelector('input')
+    input.value = 'vahsvhas'
+    dom.window.document
+        .querySelector('form')
+        .dispatchEvent(new dom.window.Event('submit'))
+
+    const h1 = dom.window.document.querySelector('h1')
+
+    assert.strictEqual(h1.innerHTML, 'Invalid email.')
+})
